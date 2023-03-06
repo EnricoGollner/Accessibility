97c624c64e2a43609dc8b850a2e8689b693f42be
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-03-06 01:07:41 -0300

    New project

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Enrico Sousa Gollner on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
